d7275472e85ceb8fefdcbcac6da5451c58ec8dad
Trenton Cornwall <trenton434@gmail.com>
2019-10-15 14:52:45 -0600

    changing url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const axios = require("axios");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
 	res.render("index");
 });
 
@@ -15,8 +15,8 @@ router.post("/create", (req, res) => {
 		`going to send a get to axios https://www.opentable.com/widget/reservation/api/restaurant?rid=${data.rid}`
 	);
 	axios(
-		`https://www.opentable.com/widget/reservation/api/restaurant?rid=${data.rid}`
-	)
+			`www.opentable.com/widget/reservation/api/restaurant?rid=${data.rid}`
+		)
 		.then(response => {
 			console.log(`getting response from axios`, response);
 			var query = `http://www.opentable.${data.region}/restaurant/profile/${data.rid}/reserve?restref=${data.rid}&datetime=${data.date}T${data.time}&covers=${data.party}&searchdatetime=${data.date}T${data.time}&partysize=${data.party}`;
@@ -24,7 +24,10 @@ router.post("/create", (req, res) => {
 				query += `&ref=${data.source}`;
 			}
 			var restref = `https://www.opentable.${data.region}/r${response.data.nlUrl}?restref=${data.rid}`;
-			res.json({ query: query, restref: restref });
+			res.json({
+				query: query,
+				restref: restref
+			});
 		})
 		.catch(error => {
 			console.log(error);
