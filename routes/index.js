59250ad1db09e364e1df7ac064ae68286ab2b24d
Trenton Cornwall <trenton434@gmail.com>
2019-10-15 18:35:29 -0600

    fixed bug when removing api

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,19 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/create", (req, res) => {
-			// creates restref url
-			var data = req.body;
-			console.log(data);
-			console.log(
-				`going to send a get to axios https://www.opentable.com/widget/reservation/api/restaurant?rid=${data.rid}`
-			);
-			var query = `http://www.opentable.${data.region}/restaurant/profile/${data.rid}/reserve?restref=${data.rid}&datetime=${data.date}T${data.time}&covers=${data.party}&searchdatetime=${data.date}T${data.time}&partysize=${data.party}`;
-			if (data.source) {
-				query += `&ref=${data.source}`;
-			}
+	// creates restref url
+	var data = req.body;
+	console.log(data);
+	console.log(
+		`going to send a get to axios https://www.opentable.com/widget/reservation/api/restaurant?rid=${data.rid}`
+	);
+	var query = `http://www.opentable.${data.region}/restaurant/profile/${data.rid}/reserve?restref=${data.rid}&datetime=${data.date}T${data.time}&covers=${data.party}&searchdatetime=${data.date}T${data.time}&partysize=${data.party}`;
+	if (data.source) {
+		query += `&ref=${data.source}`;
+	}
 
-			res.json({
-				query: query
-			});
-
-			module.exports = router;
+	res.json({
+		query: query
+	});
+})
+module.exports = router;
