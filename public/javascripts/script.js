413a1f2bbf0eca5c98d3ecb131b521cc791015b7
Trenton Cornwall <trenton434@gmail.com>
2020-01-15 12:23:11 -0700

    changed to the new source and added campaign option

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,58 +1,48 @@
 function create(theForm) {
-	event.preventDefault();
-	const region = $("#region").val();
-	const rid = $("#rid").val();
-	const date = $("#datepicker").val();
-	const time = $("#time").val();
-	const party = $("#party").val();
-	const source = $("#source").val();
+  event.preventDefault();
+  const region = $("#region").val();
+  const rid = $("#rid").val();
+  const date = $("#datepicker").val();
+  const time = $("#time").val();
+  const party = $("#party").val();
+  const source = $("#source").val();
+  const campaign = $("#campaign").val();
 
-	//
-	// Yea... I built a server... because I wanted practice
-	//
+  //
+  // Yea... I built a server... because I wanted practice
+  //
 
-	if (!region || !rid || !date || !time || !party) {
-		$("#url").text("Missing Fields...");
-	} else {
-		$.post(
-			"/create",
-			{
-				region: region,
-				rid: rid,
-				date: date,
-				time: time,
-				party: party,
-				source: source
-			},
-			data => {
-				//Initiating ClipboardJS library
-				new ClipboardJS(".btn");
-				// Creating Date Specific URL
-				let urlLabel = $("<div id='urllabel'>").append(
-					$("<p>").text("Date Specific URL:")
-				);
-				let urlField = $(`<input id='urlField' value=${data.query}>`);
-				let copyButton = $(
-					`<button class='btn' data-clipboard-target='#urlField'>`
-				).text("Copy");
+  if (!region || !rid || !date || !time || !party) {
+    $("#url").text("Missing Fields...");
+  } else {
+    $.post(
+      "/create",
+      {
+        region: region,
+        rid: rid,
+        date: date,
+        time: time,
+        party: party,
+        source: source,
+        campaign: campaign
+      },
+      data => {
+        //Initiating ClipboardJS library
+        new ClipboardJS(".btn");
+        // Creating Date Specific URL
+        let urlLabel = $("<div id='urllabel'>").append(
+          $("<p>").text("Date Specific URL:")
+        );
+        let urlField = $(`<input id='urlField' value=${data.query}>`);
+        let copyButton = $(
+          `<button class='btn' data-clipboard-target='#urlField'>`
+        ).text("Copy");
 
-				$("#urlcontainer").empty();
-				$("#urlcontainer").append(urlField, copyButton);
-				$(".urllabel").empy();
-				$(".urllabel").append(urlLabel);
-
-				// Creating RestRef URL
-				// let rrLabel = $("<div id='urllabel'>").append(
-				// 	$("<p>").text("Standard RestRef URL:")
-				// );
-				// let rrField = $(`<input id='urlField' value=${data.restref}>`);
-				// let rrcopyButton = $(
-				// 	`<button class='btn' data-clipboard-target='#rrField'>`
-				// ).text("Copy");
-				// $("#rrcontainer").empty();
-				// $("#rrcontainer").append(rrField, rrcopyButton);
-				// $(".rrlabel").append(rrLabel);
-			}
-		);
-	}
+        $("#urlcontainer").empty();
+        $("#urlcontainer").append(urlField, copyButton);
+        $(".urllabel").empy();
+        $(".urllabel").append(urlLabel);
+      }
+    );
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,24 @@ const axios = require("axios");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	res.render("index");
+  res.render("index");
 });
 
 router.post("/create", (req, res) => {
-	// creates restref url
-	var data = req.body;
+  // creates restref url
+  var data = req.body;
+  console.log(data);
 
-	var query = `http://www.opentable.${data.region}/restaurant/profile/${data.rid}/reserve?restref=${data.rid}&datetime=${data.date}T${data.time}&covers=${data.party}&searchdatetime=${data.date}T${data.time}&partysize=${data.party}`;
-	if (data.source) {
-		query += `&ref=${data.source}`;
-	}
+  var query = `http://www.opentable.${data.region}/restaurant/profile/${data.rid}/reserve?restref=${data.rid}&datetime=${data.date}T${data.time}&covers=${data.party}&searchdatetime=${data.date}T${data.time}&partysize=${data.party}`;
+  if (data.source) {
+    query += `&ot_source=${data.source}`;
+  }
+  if (data.campaign) {
+    query += `&ot_campaign=${data.campaign}`;
+  }
 
-	res.json({
-		query: query
-	});
+  res.json({
+    query: query
+  });
 });
 module.exports = router;
